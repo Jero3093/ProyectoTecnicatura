269dd24e6d2d09ae7a0af139d8b58e3b9d8de823
Jeronimo <jeronimotornini@gmail.com>
2023-06-27 20:53:05 -0300

    Cambio en fuentes de texto y en tipo de input

--- assets/Components/Menu.php
+++ assets/Components/Menu.php
@@ -2,7 +2,7 @@
 echo '
     <div class="Menu">
         <div class="Menu_Search">
-        <input type="search" name="txt_search" id="txt_search" class="Menu_Search-Input search_ecommerce" placeholder="Buscar: ">
+        <input type="text" name="txt_search" id="txt_search" class="Menu_Search-Input search_ecommerce" placeholder="Buscar: ">
             <button class="Menu_Search-Button">
                 <img src="./../assets/icons/Buscar.png" class="Menu_Search-Button-Icon" />
             </button>
